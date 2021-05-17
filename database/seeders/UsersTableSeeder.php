0fdf33da9586b743f825c7cfe0a47abd5ae8db7e
enBoig <enboig@gmail.com>
2021-05-17 18:01:07 +0200

    Addes user seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = User::factory()->make();
+            $user->email = "seller" . $i . "@example.com";
+            $user->save();
+        }
+        for ($i = 0; $i < 30; $i++) {
+            $user = User::factory()->make();
+            $user->email = "client" . $i . "@example.com";
+            $user->save();
+        }
+    }
+}
